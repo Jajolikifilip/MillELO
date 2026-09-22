18b25584ab872a84a15137e0a051f4f28ab6b248
Filip Frouth <filip.frouth@gmail.com>
2026-09-22 19:46:28 +0000

    Update app

--- MillELOcom/MillELOcom/app.py
+++ MillELOcom/MillELOcom/app.py
@@ -6,6 +6,7 @@ import sys
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY") or 'milLELO-secret-key-2024'
+app.config['PUBLIC_SITE_URL'] = os.environ.get('PUBLIC_SITE_URL', 'https://millelo.onrender.com').rstrip('/')
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
 app.config['SESSION_COOKIE_HTTPONLY'] = True
--- MillELOcom/MillELOcom/main.py
+++ MillELOcom/MillELOcom/main.py
@@ -1167,7 +1167,7 @@ def favicon():
 
 @app.route('/robots.txt')
 def robots_txt():
-    base_url = request.host_url.rstrip('/')
+    base_url = app.config['PUBLIC_SITE_URL']
     content = f"""User-agent: *
 Allow: /
 Sitemap: {base_url}/sitemap.xml
@@ -1176,7 +1176,7 @@ Sitemap: {base_url}/sitemap.xml
 
 @app.route('/sitemap.xml')
 def sitemap_xml():
-    base_url = request.host_url.rstrip('/')
+    base_url = app.config['PUBLIC_SITE_URL']
     content = f"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
